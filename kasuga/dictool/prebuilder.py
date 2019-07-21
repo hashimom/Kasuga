bf66c7fca1fcc461023bc455c05fd7d53cc44a66
Masahiko Hashimoto <hashimom@geeko.jp>
2019-07-22 00:05:44 +0900

    add trigram info function

--- kasuga/dictool/prebuilder.py
+++ kasuga/dictool/prebuilder.py
@@ -21,6 +21,7 @@
  SOFTWARE.
 """
 import argparse
+import os
 import json
 import csv
 import glob
@@ -28,9 +29,16 @@ from kasuga.reader import JsonWriter
 
 
 class PreBuilder:
-    def __init__(self, in_dir, out_file, in_text=None):
-        self.f = open(out_file, 'w')
-        self.writer = csv.writer(self.f, lineterminator='\n')
+    def __init__(self, in_dir, out_dir, in_text=None):
+        if not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
+
+        self.f_trigram = open(out_dir + "/trigram.csv", 'w')
+        self.writer_trigram = csv.writer(self.f_trigram, lineterminator='\n')
+
+        self.f_link = open(out_dir + "/link.csv", 'w')
+        self.writer_link = csv.writer(self.f_link, lineterminator='\n')
+
         self.in_dir = in_dir
         self.jw = None
         if in_text is not None:
@@ -46,52 +54,87 @@ class PreBuilder:
         for file in file_list:
             with open(file, encoding="utf-8") as in_json:
                 info = json.load(in_json)
-
                 for chunk in info["Chunks"]:
+                    # TriGram info
+                    trigram_info = self.make_trigram_info(chunk)
+                    for trigram in trigram_info:
+                        self.writer_trigram.writerow(trigram)
+
+                    # Link info
                     if len(chunk["Independent"]) != 0 and not chunk["Link"] is None:
-                        ind_phase = ""
-                        lnk_phase = ""
-                        ind_position = [None, None]
-                        ind_conjugate = [None, None]
-                        lnk_position = [None, None]
-                        lnk_conjugate = [None, None]
+                        link_info = self.make_link_info(chunk)
+                        if link_info is not None:
+                            self.writer_link.writerow(link_info)
 
-                        # Independent
-                        if len(chunk["Independent"]) == 1:
-                            ind_phase = chunk["Independent"][0]["surface"]
-                            ind_position = chunk["Independent"][0]["position_id"]
-                            ind_conjugate = chunk["Independent"][0]["conjugate_id"]
-                        else:
-                            for independent in chunk["Independent"]:
-                                ind_phase += independent["surface"]
+    @staticmethod
+    def make_trigram_info(chunk):
+        trigram = []
+        words = chunk["Independent"] + chunk["Ancillary"]
+        chunk_len = len(words)
+        if chunk_len != 0:
+            if chunk_len == 1:
+                trigram.append(["@SOC@", 0, 0,
+                                words[0]["surface"], words[0]["position_id"][0], words[0]["position_id"][1],
+                                "@EOC@", 0, 0])
+            else:
+                for i, word in enumerate(words):
+                    if i == 0:
+                        tri_word = ["@SOC@", 0, 0]
+                    else:
+                        tri_word = [words[i - 1]["surface"],
+                                    words[i - 1]["position_id"][0],
+                                    words[i - 1]["position_id"][1]]
 
-                        # Ancillary
-                        if len(chunk["Ancillary"]) == 1 and chunk["Ancillary"][0]["position"][0] != "特殊":
-                            anc_phase = chunk["Ancillary"][0]["surface"]
-                            anc_position = chunk["Ancillary"][0]["position_id"]
-                            anc_conjugate = chunk["Ancillary"][0]["conjugate_id"]
-                        else:
-                            continue
+                    tri_word.extend([word["surface"], word["position_id"][0], word["position_id"][1]])
 
-                        # Link
-                        if len(chunk["Ancillary"]) == 1:
-                            lnk_phase = chunk["Link"][0]["surface"]
-                            lnk_position = chunk["Link"][0]["position_id"]
-                            lnk_conjugate = chunk["Link"][0]["conjugate_id"]
-                        else:
-                            for i, link in enumerate(chunk["Link"]):
-                                if i == 0:
-                                    if link["position"][0] == "動詞" or link["position"][0] == "形容詞":
-                                        lnk_phase = link["surface"]
-                                        lnk_position = link["position_id"]
-                                        lnk_conjugate = link["conjugate_id"]
-                                        break
-                                lnk_phase += link["surface"]
+                    if (i + 1) == chunk_len:
+                        tri_word.extend(["@EOC@", 0, 0])
+                    else:
+                        tri_word.extend([words[i + 1]["surface"],
+                                         words[i + 1]["position_id"][0],
+                                         words[i + 1]["position_id"][1]])
 
-                        self.writer.writerow([ind_phase, anc_phase, lnk_phase,
-                                              ind_position[0], ind_position[1], ind_conjugate[0], ind_conjugate[1],
-                                              anc_position[0], anc_position[1], anc_conjugate[0], anc_conjugate[1],
-                                              lnk_position[0], lnk_position[1], lnk_conjugate[0], lnk_conjugate[1]])
+                    trigram.append(tri_word)
+
+        return trigram
+
+    @staticmethod
+    def make_link_info(chunk):
+        ind_phase = ""
+        lnk_phase = ""
+        ind_position = [None, None]
+        lnk_position = [None, None]
+
+        # Independent
+        if len(chunk["Independent"]) == 1:
+            ind_phase = chunk["Independent"][0]["surface"]
+            ind_position = chunk["Independent"][0]["position_id"]
+        else:
+            for independent in chunk["Independent"]:
+                ind_phase += independent["surface"]
+
+        # Ancillary
+        if len(chunk["Ancillary"]) == 1 and chunk["Ancillary"][0]["position"][0] != "特殊":
+            anc_phase = chunk["Ancillary"][0]["surface"]
+            anc_position = chunk["Ancillary"][0]["position_id"]
+        else:
+            return None
+
+        # Link
+        if len(chunk["Ancillary"]) == 1:
+            lnk_phase = chunk["Link"][0]["original"]
+            lnk_position = chunk["Link"][0]["position_id"]
+        else:
+            for i, link in enumerate(chunk["Link"]):
+                if i == 0:
+                    if link["position"][0] == "動詞" or link["position"][0] == "形容詞":
+                        lnk_phase = link["original"]
+                        lnk_position = link["position_id"]
+                        break
+                lnk_phase += link["original"]
+
+        return [ind_phase, anc_phase, lnk_phase, ind_position[0], ind_position[1],
+                anc_position[0], anc_position[1], lnk_position[0], lnk_position[1]]
 
 
 def main():
